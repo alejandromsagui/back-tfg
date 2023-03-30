4c8c587b786ce4a2c451c707d3ffe769f92bc71c
alejandromsagui <alex.maeso19@gmail.com>
2023-03-30 09:04:36 +0200

    Añadida la cadena de conexión con mongodb Atlas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter = require('./routes/userRouter');
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '.env' });
 
 const app = express();
 const port = 3000;
@@ -13,7 +13,7 @@ app.listen(port, () => {
 
 //Conexión a la base de datos
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(db => console.log('Conexión con la base de datos establecida'))
     .catch(err => console.log('Error: ', err));
 
