ab89c28d87683a2f8f0b6d4e3348bd721aa73d8d
alejandromsagui <alex.maeso19@gmail.com>
2023-03-31 16:03:40 +0200

    Añadido los métodos del controlador de videojuegos sin probar

--- src/controllers/videogameController.js
+++ src/controllers/videogameController.js
@@ -41,4 +41,53 @@ const newVideogame = async (req, res) => {
     }
 }
 
-module.exports = { getVideogames, newVideogame }
+const updateVideogame = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const update = {}
+        const videogame = await Videogame.findById(id);
+
+        if (!videogame) {
+            return res.status(404).send({
+                message: 'El videojuego no existe'
+            })
+        }
+
+        const updateVideogame = await Videogame.findByIdAndUpdate(id, update, { new: true });
+
+        return res.status(200).json({
+            message: 'El usuario se ha actualizado correctamente',
+            updateVideogame
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Error al actualizar los datos del videojuego',
+            err
+        })
+    }
+}
+
+const deleteVideogame = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const videogame = await Videogame.findByIdAndDelete(id);
+
+        if (!videogame) {
+            return res.status(404).send({
+                message: 'El videojuego no existe'
+            })
+        }
+
+        return res.status(200).send({
+            message: 'El videojuego ha sido eliminado correctamente'
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Error en el servidor',
+            err
+        });
+    }
+}
+
+module.exports = { getVideogames, newVideogame, updateVideogame, deleteVideogame }
--- src/routes/videogameRouter.js
+++ src/routes/videogameRouter.js
@@ -4,6 +4,8 @@ const videogameController = require('../controllers/videogameController');
 
 videogameRouter.get('/videogames', videogameController.getVideogames);
 videogameRouter.post('/newVideogame', videogameController.newVideogame);
+videogameRouter.put('/updateVideogame/:id', videogameController.updateVideogame);
+videogameRouter.delete('/deleteVideogame/:id', videogameController.deleteVideogame);
 
 
 module.exports = videogameRouter
