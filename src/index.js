2901fdac37f3523dd9e11483ebe7892f3cad88c4
alejandromsagui <alex.maeso19@gmail.com>
2023-04-22 18:42:21 +0200

    Probando primer despliegue en Heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const authRouter = require('./routes/auth');
 const validateToken = require('./middlewares/validate-token')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 //Servidor en escucha
