5b7caf87bb3bb0b6234865165f105fd17a399d3a
alejandromsagui <alex.maeso19@gmail.com>
2023-05-08 00:06:25 +0200

    Añadidos algunos cambios

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { multerMid } = require('./middlewares/multer')
 const {reduceImageSize} = require('./middlewares/multer')
-const uploadImage = require('./helpers/upload')
 const errorHandler = require('./middlewares/errorHandler')
 
 //Servidor en escucha
@@ -33,26 +32,9 @@ app.use(cors({
     credentials: true
 }))
 
-
-
 //Rutas
 app.use(userRouter);
 app.use(videogameRouter);
 app.use(authRouter);
 
-//Ruta subida de imágenes
-app.post('/uploads', async (req, res, next) => {
-    try {
-        const myFile = req.file
-        const imageUrl = await uploadImage(myFile)
-        res
-            .status(200)
-            .json({
-                message: "Subida de imagen completada",
-                data: imageUrl
-            })
-    } catch (error) {
-        next(error)
-    }
-})
 
