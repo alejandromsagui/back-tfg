a056eabe65197e63f234ccf6d672008b5620b369
alejandromsagui <alex.maeso19@gmail.com>
2023-05-31 00:26:59 +0200

    prueba de nuevas cabeceras

--- src/index.js
+++ src/index.js
@@ -60,24 +60,14 @@ app.use(errorHandler)
 //     credentials: true
 // }))
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://namekiansgames.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://namekiansgames.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
+
 
 app.post('/uploadkk', async (file) => {
     try {
